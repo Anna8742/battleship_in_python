2ce5d2a6572b8d10e3b9343ebb20c13a221571db
Anna8742 P <anna.papka014@gmail.com>
2024-04-03 12:18:06 +0000

    create grid, secondary and primary board

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+from random import randint
+
+rules = '''RULES: \nThe game is played on four grids, two for each player. 
+\nThe grids are square 10x10, and the individual squares in the grid are identified by letter and number.
+\nOne one grid the player arranges ships and records the shots by the opponent. on the other, hidden grid, the player records their own shots.
+\n Before play begins, the ships are secretly arranged on the primary grid. The types of numbers of ships are allowed are the same for each player.'''
+print(rules)
+
+grid = {'A' : 0, 'B' : 1, 'C' : 2, 'D' : 3, 'E' : 4, 'F' : 5, 'G' : 6, 'H' : 7, 'I' : 8, 'J' : 9}
+
+
+first_board = [[" "]] * 9 for x in range(9)]
+
+record_board = [[" "]] * 9 for x in range(9)]
+
+
+
+
+
