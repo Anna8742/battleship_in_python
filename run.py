30b625b1a1596d0a89c929eda4ced10f874c95c2
Anna8742 P <anna.papka014@gmail.com>
2024-04-04 17:23:34 +0000

    renamed functions to be more descriptive from create_primary_board to print board; adjust the display format of the grid, improved feedback for the player; implemented error handling

--- run.py
+++ run.py
@@ -11,51 +11,25 @@ print(rules)
 BOARD_SIZE = 10
 NUMBER_OF_SHIPS = 5
 
-
-
 # Mapping of letters to grids
 grid = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9}
 
 # Initialize player's primary board
-player_board = [[" "] * 10 for _ in range(10)]
+player_board = [[" "] * BOARD_SIZE for _ in range(BOARD_SIZE)]
 
 # Initialize computer's primary board
-computer_board = [[" "] * 10 for _ in range(10)]
+computer_board = [[" "] * BOARD_SIZE for _ in range(BOARD_SIZE)]
 
 
-def create_primary_board(board, ships=None, hits=None, misses=None, user_guess=None):
+def print_board(board):
     """
-    Function to create and print the primary game board.
-    As arguments it takes 'board' as an input to display primary board,
-    'ships', 'hits', and 'misses' to display them on the board. The ships are
-    represented by "S", hits by "H" and misses by "M" on the board.
+    Function to print the game board.
     """
     print('  A B C D E F G H I J ')
     print('  +-+-+-+-+-+-+-+-+-+ ')
-    number = 1
-    ships = set() if ships is None else ships
-    hits = set() if hits is None else hits
-    misses = set() if misses is None else misses
-    user_guess = set() if user_guess is None else user_guess
     for i, row in enumerate(board):
-        display_row = []
-        for j, cell in enumerate(row):
-            if (i, j) in ships:
-                # Ship
-                display_row.append("S")
-            elif (i, j) in hits:
-                # Hit
-                display_row.append("H")
-            elif (i, j) in misses:
-                # Miss
-                display_row.append("M")
-            elif (i, j) in user_guess:
-                # User Guess
-                display_row.append("X")
-            else:
-                display_row.append(cell)
-        print("%2d|%s|" % (number, "|".join(display_row)))
-        number += 1
+        print("%2d|%s|" % (i + 1, "|".join(row)))
+    print('  +-+-+-+-+-+-+-+-+-+ ')
 
 
 def user_guess():
@@ -67,7 +41,7 @@ def user_guess():
         try:
             print("Enter your guess")
             row = int(input("Enter the number from 1 to 10: ")) - 1
-            if row not in range(10):
+            if row not in range(BOARD_SIZE):
                 raise ValueError("Row must be between 1 and 10.")
             column = input("Enter a letter from A to J: ").upper()
             if column not in "ABCDEFGHIJ":
@@ -80,35 +54,33 @@ def user_guess():
 def AI_guess():
     """
     Function to generate a random guess for the computer's turn.
-    It returns of tuple of the computer's guess
+    It returns a tuple of the computer's guess.
     """
-    row = randint(0, 9)
-    column = randint(0, 9)
-    return row, column
+    return randint(0, BOARD_SIZE - 1), randint(0, BOARD_SIZE - 1)
 
 
-def check_result(guess, board, hits, misses, user_guess):
+def check_result(guess, board, hits, misses):
     """
     Function to check the result of a guess made by computer and player.
     It takes arguments: guess, board - to check against it, sets of hits and misses.
     It updates sets based on the result and returns True for a hit and False for a miss.
     """
     row, column = guess
-    if board[row][column] == "X":
+    if board[row][column] == "S":
         print("Hit")
         hits.add((row, column))
-        board[row][column] = "H"
+        board[row][column] = "X"
         return True
     else:
         print("Miss! ")
         misses.add((row, column))
-        user_guess.add((row, column))
+        board[row][column] = "M"
         return False
 
 
 def place_battleships(board):
     """
-    This funcion locates battleships on the board.
+    This function locates battleships on the board.
     It takes the board as an argument and returns a set of the placed ships.
     """
     ships = set()
@@ -135,41 +107,37 @@ def main():
             player_misses = set()
             computer_hits = set()
             computer_misses = set()
-            player_guesses = set()
 
             # Game loop
-            while len(player_hits) < 5 and len(computer_hits) < 5 and len(player_misses) < 10:
+            while len(player_hits) < NUMBER_OF_SHIPS and len(computer_hits) < NUMBER_OF_SHIPS and len(player_misses) < BOARD_SIZE:
                 # Player's turn
-                print("\nPlayer's Turn:")
-                print("\nPlayer's guesses on computer's board: ")
-                create_primary_board(computer_board, hits=player_hits, misses=player_misses, user_guess=player_guesses)
+                print("\nYour Turn:")
+                print_board(computer_board)
                 user_guess_coord = user_guess()
-                if check_result(user_guess_coord, computer_board, computer_hits, computer_misses, player_guesses):
-                    print("\nPlayer's Primary Board: ")
-                    create_primary_board(player_board, ships=player_ships, hits=computer_hits, misses=computer_misses)
+                if check_result(user_guess_coord, computer_board, player_hits, player_misses):
+                    print("Your guess:")
+                    print_board(computer_board)
                 else:
-                    print("\nPlayer's Primary Board: ")
-                    create_primary_board(player_board, ships=player_ships, hits=computer_hits, misses=computer_misses)
+                    print("Your guess:")
+                    print_board(computer_board)
 
-                if len(computer_hits) == 5:
-                    print("\nCongratulations! You sank all the computer's battleships!")
+                if len(computer_hits) == NUMBER_OF_SHIPS:
+                    print("Congratulations! You sank all of the computer's battleships!")
                     break
 
                 # Computer's turn
                 print("\nComputer's Turn:")
                 computer_guess_coord = AI_guess()
-                print("Computer guesses:", computer_guess_coord)
-                if check_result(computer_guess_coord, player_board, player_hits, player_misses, player_guesses):
-                    print("\nPlayer's Primary Board and computer's guesses: ")
-                    create_primary_board(player_board, ships=player_ships, hits=player_hits, misses=player_misses,
-                                         user_guess=player_guesses)
+                print("Computer's guess:", chr(computer_guess_coord[1] + 65) + str(computer_guess_coord[0] + 1))
+                if check_result(computer_guess_coord, player_board, computer_hits, computer_misses):
+                    print("Computer's guess:")
+                    print_board(player_board)
                 else:
-                    print("\nPlayer's Primary Board and computer's guesses: ")
-                    create_primary_board(player_board, ships=player_ships, hits=player_hits, misses=player_misses,
-                                         user_guess=player_guesses)
+                    print("Computer's guess:")
+                    print_board(player_board)
 
-                if len(player_hits) == 5:
-                    print("\nOops! All your battleships have been sunk by the computer!")
+                if len(player_hits) == NUMBER_OF_SHIPS:
+                    print("Oops! All of your battleships have been sunk by the computer!")
                     break
 
             # Play again
@@ -178,7 +146,6 @@ def main():
                 print("Thank you for playing! Goodbye!")
                 break
         except Exception as e:
-            print("Error:", e)
             print("An error occurred. Restarting the game.")
 
 
